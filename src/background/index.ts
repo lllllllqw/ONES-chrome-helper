303e9f1cf91ea6859a85360c9dc476dabda54e0d
lllllllqw <465633856@qq.com>
2020-06-13 14:55:52 +0800

    add: 拦截并修改请求头（失败

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,25 @@
 console.log('This is background page!');
 
+// 没用啊。。
+// chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
+//     if(details.requestHeaders) {
+//         details.requestHeaders.push({
+//             name: 'x-ones-api-branch-project',
+//             value: '/project/master/'
+//         })
+//         console.log("onBeforeSendHeaders details.requestHeaders", details.requestHeaders)
+//     }
+//     return {requestHeaders: details.requestHeaders};
+// }, {
+//     urls: ['<all_urls>']
+// }, ['requestHeaders', 'extraHeaders'])
+
+// chrome.webRequest.onSendHeaders.addListener((details) => {
+//     if(details.requestHeaders) {
+//         console.log("onSendHeaders details.requestHeaders", details.requestHeaders)
+//     }
+// }, {
+//     urls: ['<all_urls>']
+// }, ['requestHeaders'])
+
 export default null;
